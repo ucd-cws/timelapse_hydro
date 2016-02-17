6723430706916d0e9de5a953a943e9838b536eab
Ryan Peek <rapeek@ucdavis.edu>
2016-02-17 01:48:00 -0800

    update exif script

--- scripts/exiftools.R
+++ scripts/exiftools.R
@@ -1,5 +1,8 @@
-## Some notes on using exiftool
+## USE EXIFTOOL TO PULL PHOTO METADATA
+## 2016-02-17
+## R. Peek
 
+## Some notes on using exiftool
 ## this line will pull photo name, creation date, and exposure/gamecam strip info
 ## It's fast and requires "exiftools"
 ##@ exiftool -r -s -T -FILE:FileName -EXIF:CreateDate -EXIF:Software -EXIF:ImageDescription . > photoinfo/exifinfo.txt"
@@ -15,36 +18,44 @@
 ## exiftool -r -s -G -EXIF:ModifyDate PICT0001.JPG
 
 
+# LIBRARIES ---------------------------------------------------------------
+
+library(readr)
+library(lubridate)
+library(tidyr)
+
 # OLD MOULTRIE ------------------------------------------------------------
 
-
 path.to.photos<-"/gamecam/"
-site <- "NFA"
+path.to.photos<-"/Volumes/projects/Long_Term_Monitoring/American_Yuba_Rivers/photos/NFYuba/TimeLapse/20140512/" # server
+site <- "NFY"
 
 # internal R call for old MOULTRIE
 exifinfo<-system(command = paste0('exiftool -r -s -T -FILE:FileName -EXIF:CreateDate -EXIF:Software -EXIF:ImageDescription ',path.to.photos),intern=T)
 
-# read in file (quicker and easier this way...make separate system calls to write file
-# then pull and massage as needed).
+# make a text file from metadata
+text <- as.data.frame(row.names = NULL, x = exifinfo, stringsAsFactors = F) # convert to text dataframe with column name
 
-library(readr)
-tst<-read_tsv(file = paste0(root, "PROJECTS/timelapse_hydro/data/exifinfo.txt"), 
-              col_names = c("photo","datetime","exposure","infostrip"))
-h(tst)
+# split out data into columns by tab
+df<-separate(data=text, col = exifinfo, into = c("photo", "datetime", "exposure","infostrip"), sep = "\t") # split by tab
 
-# get just exposure
-tst$exposure <- stringr::str_sub(tst[,"exposure"], -3, -1)
-tst$baro_inHg <- as.numeric(stringr::str_extract(tst[,"infostrip"],pattern="([1-9]{2})[.]([0-9]{2})"))
-tst$air_C <- as.numeric(stringr::str_extract(tst[,'infostrip'],pattern='[.]([1-9]{1,2})[*C]'))
-tst$site <- site
-tst<-tst[,c(6,1:3,5)] # pull cols of interest
+# now format columns
+df$exposure <- as.numeric(stringr::str_sub(df[,"exposure"], -3, -1)) # get just exposure
+df$datetime<-ymd_hms(df$datetime) # convert datetime to POSIXct
+df$baro_inHg <- as.numeric(stringr::str_extract(df[,"infostrip"],pattern="([1-9]{2})[.]([0-9]{2})"))
+df$air_C <- as.numeric(stri_sub(sapply(strsplit(df$infostrip, ".", fixed = T), "[", 4),from = 1, -4))
+#df$air_C <- as.numeric(stringr::str_extract(df[,'infostrip'],pattern='(?:inHg.\\s.)([0-9]{1,2})[.C]'))
+df$site <- site
+df<-df[,c(7,1:3,5,6)] # pull cols of interest
+str(df)
 
 df.name<-paste0(site, "_exif") # set name for assigning to dataframe
-assign(df.name, tst) # assign dataframe
+assign(df.name, df) # assign dataframe
 
 # save data
-save(NFA_exif, file = paste0(root, "PROJECTS/timelapse_hydro/data/", site, "_exif.rda")) # save to rda
-write_rds(assign(df.name, tst), path = paste0(root, "PROJECTS/timelapse_hydro/data/", site, "_exif.rds")) # save to rds
+#save(NFA_exif, file = paste0(root, "PROJECTS/timelapse_hydro/data/", site, "_exif.rda")) # save to rda
+write_rds(assign(df.name, df), path = paste0("./data/", site, "_exif.rds")) # save to rds
+write_rds(assign(df.name, df), path = paste0("./data/", site, "_exif_20140512.rds")) # for specific date
 
 # read in rds
 #nfa_exif<-read_rds(path = paste0(root, "PROJECTS/timelapse_hydro/data/", site, "_exif.rds"))
@@ -52,17 +63,12 @@ write_rds(assign(df.name, tst), path = paste0(root, "PROJECTS/timelapse_hydro/da
 # read in rda
 #load(file = paste0(root, "PROJECTS/timelapse_hydro/data/", site, "_exif.rda"))
 
-
-
 # NEW MOULTRIE ------------------------------------------------------------
 
-library(tidyr) # use tidyr to split into cols
-library(lubridate)
-library(readr)
-
 # photo location and site info
-path.to.photos<-"I:/DCIM/100MFCAM"
-site <- "OFF"
+path.to.photos<-"I:/DCIM/100MFCAM" # SD Card
+path.to.photos<-"/Volumes/projects/Long_Term_Monitoring/American_Yuba_Rivers/photos/NFYuba/TimeLapse/20140512/" # server
+site <- "NFY"
 
 # internal R call for new MOULTRIE, get info and write to local dataframe in R
 exifinfo<-system(command = paste0('exiftool -r -s -T -FILE:FileName -EXIF:CreateDate -EXIF:MakerNoteUnknownText ',path.to.photos),intern=T)
