a159821fd1db81249083deca98717f8d499dfc7d
Ryan Peek <rapeek@ucdavis.edu>
2016-05-23 01:05:02 -0700

    updated with MFY scripts and update functions

--- scripts/functions/f_hydrographs.R
+++ scripts/functions/f_hydrographs.R
@@ -1,33 +1,90 @@
 ## make hydrographs
 
-fig.Hydro <- function(test.subset = nrow(photolist_sub)){
+fig.Hydro <- function(test.subset = nrow(photolist_sub), stage=TRUE, cms=FALSE, yvar="lev.avg"){
   
-  print("Making Hydrographs only")
-  for(j in 1:nrow(photolist_sub[c(1:test.subset),])){
-    
-    print(paste("making graph", j, "of", nrow(photolist_sub)))
-    #print(paste("making graph", j, "of", test.subset))
-    
-    png(paste(getwd(), "/output/temp/hydro/", photolist_sub[j,"photo"], sep = ""), 
-        height=4, width=6, units="in", res=500, family="sans", bg = "transparent")
-    
-    grid.arrange(
-      ggplot(dff, aes(x = datetime, y = lev.avg)) + geom_line(color = "grey30", size = 1) + theme_bw() + 
-        geom_ribbon(data = dff[dff$datetime <= photolist_sub$timeround[j],], 
-                    aes(ymax = lev.avg, ymin = min(dff$lev.avg)), fill = "blue", alpha = .5) +
-        geom_line(data = dff[dff$datetime <= photolist_sub$timeround[j],], 
-                  aes(x = datetime, y = lev.avg), size = 2) +
-        geom_point(data = dff[dff$datetime == photolist_sub$timeround[j],], 
-                   aes(x = datetime, y = lev.avg), pch=21, fill="skyblue1", size = 8) +
-        labs(list(x = "", y = "Stage (m)")) +
-        theme(axis.text.x = element_text(face="bold", size=12),
-              axis.text.y = element_text(face="bold", size=12),
-              axis.title.y = element_text(face="bold", size=12),
-              panel.background = element_rect(fill = "transparent",colour = NA),
-              panel.grid.minor = element_blank(), 
-              panel.grid.major = element_blank(),
-              plot.background = element_rect(fill = "transparent",colour = NA))
-    )
-    dev.off()
+  if(stage){
+    print("Making Stage Hydrographs (m)")
+    for(j in 1:nrow(photolist_sub[c(1:test.subset),])){
+      
+      print(paste("making graph", j, "of", nrow(photolist_sub)))
+      
+      png(paste(getwd(), "/output/temp/hydro/", photolist_sub[j,"photo"], sep = ""), 
+          height=4, width=6, units="in", res=500, family="sans", bg = "transparent")
+      
+      grid.arrange(
+        ggplot(dff, aes_string(x = "datetime", y = yvar)) + geom_line(color = "grey30", size = 1) + theme_bw() + 
+          geom_ribbon(data = dff[dff$datetime <= photolist_sub$timeround[j],], 
+                      aes_string(ymax = yvar, ymin = min(dff[[yvar]])), fill = "blue", alpha = .5) +
+          geom_line(data = dff[dff$datetime <= photolist_sub$timeround[j],], 
+                    aes_string(x = "datetime", y = yvar), size = 2) +
+          geom_point(data = dff[dff$datetime == photolist_sub$timeround[j],], 
+                     aes_string(x = "datetime", y = yvar), pch=21, fill="skyblue1", size = 8) +
+          labs(list(x = "", y = "Stage (m)")) +
+          theme(axis.text.x = element_text(face="bold", size=12),
+                axis.text.y = element_text(face="bold", size=12),
+                axis.title.y = element_text(face="bold", size=12),
+                panel.background = element_rect(fill = "transparent",colour = NA),
+                panel.grid.minor = element_blank(), 
+                panel.grid.major = element_blank(),
+                plot.background = element_rect(fill = "transparent",colour = NA))
+      )
+      dev.off()
+    }
+  } else if(cms==FALSE & stage==FALSE) {
+    print("Making Flow Hydrographs (cfs)")
+    for(j in 1:nrow(photolist_sub[c(1:test.subset),])){
+      
+      print(paste("making graph", j, "of", nrow(photolist_sub)))
+      
+      png(paste(getwd(), "/output/temp/hydro/", photolist_sub[j,"photo"], sep = ""), 
+          height=4, width=6, units="in", res=500, family="sans", bg = "transparent")
+      
+      grid.arrange(
+        ggplot(dff, aes_string(x = "datetime", y = yvar)) + geom_line(color = "grey30", size = 1) + theme_bw() + 
+          geom_ribbon(data = dff[dff$datetime <= photolist_sub$timeround[j],], 
+                      aes_string(ymax = yvar, ymin = min(dff[[yvar]])), fill = "blue", alpha = .5) +
+          geom_line(data = dff[dff$datetime <= photolist_sub$timeround[j],], 
+                    aes_string(x = "datetime", y = yvar), size = 2) +
+          geom_point(data = dff[dff$datetime == photolist_sub$timeround[j],], 
+                     aes_string(x = "datetime", y = yvar), pch=21, fill="skyblue1", size = 8) +
+          labs(list(x = "", y = "Discharge (cfs)")) +
+          theme(axis.text.x = element_text(face="bold", size=12),
+                axis.text.y = element_text(face="bold", size=12),
+                axis.title.y = element_text(face="bold", size=12),
+                panel.background = element_rect(fill = "transparent",colour = NA),
+                panel.grid.minor = element_blank(), 
+                panel.grid.major = element_blank(),
+                plot.background = element_rect(fill = "transparent",colour = NA))
+      )
+      dev.off()
+    }
+  } else if(cms==TRUE & stage==FALSE) {
+    print("Making Flow Hydrographs (cms)")
+    for(j in 1:nrow(photolist_sub[c(1:test.subset),])){
+      
+      print(paste("making graph", j, "of", nrow(photolist_sub)))
+      
+      png(paste(getwd(), "/output/temp/hydro/", photolist_sub[j,"photo"], sep = ""), 
+          height=4, width=6, units="in", res=500, family="sans", bg = "transparent")
+      
+      grid.arrange(
+        ggplot(dff, aes_string(x = "datetime", y = yvar)) + geom_line(color = "grey30", size = 1) + theme_bw() + 
+          geom_ribbon(data = dff[dff$datetime <= photolist_sub$timeround[j],], 
+                      aes_string(ymax = yvar, ymin = min(dff[[yvar]])), fill = "blue", alpha = .5) +
+          geom_line(data = dff[dff$datetime <= photolist_sub$timeround[j],], 
+                    aes_string(x = "datetime", y = yvar), size = 2) +
+          geom_point(data = dff[dff$datetime == photolist_sub$timeround[j],], 
+                     aes_string(x = "datetime", y = yvar), pch=21, fill="skyblue1", size = 8) +
+          labs(list(x = "", y = "Discharge (cms)")) +
+          theme(axis.text.x = element_text(face="bold", size=12),
+                axis.text.y = element_text(face="bold", size=12),
+                axis.title.y = element_text(face="bold", size=12),
+                panel.background = element_rect(fill = "transparent",colour = NA),
+                panel.grid.minor = element_blank(), 
+                panel.grid.major = element_blank(),
+                plot.background = element_rect(fill = "transparent",colour = NA))
+      )
+      dev.off()
+    }
   }
 }
--- scripts/functions/f_photoComposite_osx.R
+++ scripts/functions/f_photoComposite_osx.R
@@ -2,10 +2,10 @@
 
 photoComposite <- function(bgcol = "white",
                            alpha = 0.5,
-                           plotwidth = 600,
-                           plotheight = 400,
+                           plotwidth = 800,
+                           plotheight = 600,
                            gap = 5,
-                           infobarheight = 105,
+                           infobarheight = 100,
                            plotlocation = "southeast",
                            site="",
                            thermo=TRUE,
@@ -28,8 +28,8 @@ photoComposite <- function(bgcol = "white",
       } else {
         figfold<-"hydro"
       }
-      
-      system(command = paste("convert -size 2048x1536", " ", path, '//photos//',site,'//', k, # copied photos to proj dir
+      system(command = paste("convert -size 2848x2136", " ", path, '//photos//',site,'//', k, #
+      #system(command = paste("convert -size 2048x1536", " ", path, '//photos//',site,'//', k, # copied photos to proj dir
                              ## Plot and background rectangle placement
                              ' -draw', ' " fill ', 
                              "rgba(" , col2rgb(bgcol)[1], ",", col2rgb(bgcol)[2], ",", col2rgb(bgcol)[3], ",", alpha, ")",
@@ -50,10 +50,10 @@ photoComposite <- function(bgcol = "white",
                                                                    " -gravity southwest -geometry ", plotwidth, "x", plotheight, "+", gap, "+", infobarheight + gap, " -composite",sep = "")
                              },
                              
-                             if(plotlocation == "southeast"){paste(' rectangle ', 2048 - plotwidth, ",", 1536 - (plotheight + infobarheight), " ",
-                                                                   2048 - gap, ",", 1536 + - (gap + infobarheight), '"', 
+                             if(plotlocation == "southeast"){paste(' rectangle ', 2848 - infobarheight, ",", 1946, " ",
+                                                                   1948, ",", 1436, '"', 
                                                                    " ", path, '//output//temp//', figfold,'//', k,
-                                                                   " -gravity southeast -geometry ", plotwidth, "x", plotheight, "+", gap, "+", infobarheight + gap, " -composite",sep = "")
+                                                                   " -gravity southeast -geometry ", plotwidth, "x", plotheight, "+", infobarheight, "+", infobarheight, " -composite",sep = "")
                              },
                              
                              ## Output composite image  
@@ -92,8 +92,8 @@ photoComposite <- function(bgcol = "white",
                                                                    " -gravity southwest -geometry ", plotwidth, "x", plotheight, "+", gap, "+", infobarheight + gap, " -composite",sep = "")
                              },
                              
-                             if(plotlocation == "southeast"){paste(' rectangle ', 2048 - plotwidth, ",", 1536 - (plotheight + infobarheight), " ",
-                                                                   2048 - gap, ",", 1536 + - (gap + infobarheight), '"', 
+                             if(plotlocation == "southeast"){paste(' rectangle ', 2048 - plotwidth, ",", 1536 - infobarheight, " ",
+                                                                   2048 - (plotwidth + infobarheight), ",", 1536 - (plotheight + infobarheight), '"', 
                                                                    " ", path, '//output//temp//', figfold,'//', k,
                                                                    " -gravity southeast -geometry ", plotwidth, "x", plotheight, "+", gap, "+", infobarheight + gap, " -composite",sep = "")
                              },
